453d73192bc9bb526b44de5f6eef946994e705bf
yangzecai <879894445@qq.com>
2021-11-03 15:50:35 +0800

    Refactor make_record

--- src/observer/storage/common/table.h
+++ src/observer/storage/common/table.h
@@ -107,8 +107,9 @@ class Table {
 
  private:
   RC init_record_handler(const char* base_dir);
-  RC make_record(int value_num, int tuple_num, const Value* values,
-                 std::vector<char*>& record_out);
+  RC make_record(int value_num, const Value* values, char*& record_out);
+  RC make_records(int value_num, int tuple_num, const Value* values,
+                  std::vector<char*>& record_out);
 
  private:
   Index* find_index(const char* index_name) const;
